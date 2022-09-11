233bebd27ceec2cca2d8794be2f6b4ca71979d71
Daniel Mulama <echibininow@outlook.com>
2022-09-11 08:51:11 -0700

    task 4 & 5 update 1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * Main prints "with proper grammar, but the outcome is a piece of art, on
+ * Main - prints "with proper grammar, but the outcome is a piece of art, on
  * a new line
  * Return:zero if exited properly
  */
