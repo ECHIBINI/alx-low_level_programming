6f6217e0ceb8bde20dc7b645befbac79812adbee
Daniel Mulama <echibininow@outlook.com>
2022-09-12 22:03:30 +0300

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * prints the alphabet in lowercase, followed by a new line.
+ * return(0)
+ */
+int main(void)
+
+{
+	char letter;
+
+	for (letter ='a'; letter <='z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	Return(0)
+}
