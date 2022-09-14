cb71e23ec99406383eff4c09eb226854d0d6823f
Daniel Mulama <echibininow@outlook.com>
2022-09-14 15:17:11 +0300

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * prints the alphabet, in lowercase, followed by a new line
+ * return nothing
+ */
+void print_alphabet(void);
+{
+	char c
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c)
+	}
+	_putchar('\n');
+}
