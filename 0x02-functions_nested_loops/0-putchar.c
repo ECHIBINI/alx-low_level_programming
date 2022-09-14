a9409313dba682d4fefad344480bf1fc16dc0f5f
Daniel Mulama <echibininow@outlook.com>
2022-09-14 14:35:19 +0300

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * program that prints _putchar, followed by a new line.
+ * Return 0 
+ */
+int _putchar(int char )
+{
+	_putchar(_putchar);
+	_putchar('\n');
+
+	return(0)
+}
