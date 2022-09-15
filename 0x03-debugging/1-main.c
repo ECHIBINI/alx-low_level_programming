4f2943e522ea72a34c642d67b57ab04ababd99a7
Daniel Mulama <echibininow@outlook.com>
2022-09-15 22:11:35 +0300

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,16 +9,16 @@ int main(void)
 {
         int i;
 
-        printf("Infinite loop incoming :(\n"); // prints out the statement 'Infinite loop incoming followed by a newline
+        printf("Infinite loop incoming :(\n"); 
 
         i = 0;
-
-	while (i < 10) // executes the code below as long as i is less than 10
-	{
-	putchar(i);
-	}
-
-	printf("Infinite loop avoided! \\o/\n"); // prints 'infinite loop avoided
+/*
+*	while (i < 10); 
+*	{
+*	putchar(i);
+*	}
+*/
+	printf("Infinite loop avoided! \\o/\n"); 
 
         return (0);
 }
