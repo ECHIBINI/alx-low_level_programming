2d8caf97af55991f211a4a1ad94d46a1c04a707c
Daniel Mulama <echibininow@outlook.com>
2022-09-27 21:51:02 +0300

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,13 @@
  * Return: pointer to the memory block
  */
 
-char *_memset(char *s, char b, unsigned int n)
-{
-	while (n)
-	{
-	s[n - 1] = b;
-	n--;
-	}
-	return (s);
+void *_memset(void *s, int c, size_t n)                                                                                              
+{                                                                                                                                    
+	unsigned int index;                                                                                                          
+        unsigned char *memory = s, value = c;                                                                                        
+		                                                                                                                                     
+	for (index = 0; index < n; index++)                                                                                          
+	memory[index] = value;                                                                                               
+	                                                                                                                                     
+	return (memory);                                                                                                             
 }
